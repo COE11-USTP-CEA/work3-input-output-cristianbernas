b1724249a39c9690e6eb00f4473d13f8063b72a9
Cristian C. Bernas <xianbernas@gmail.com>
2017-01-26 16:57:14 +0800

    Update project3ioBonus.java

--- project3ioBonus.java
+++ project3ioBonus.java
@@ -1,26 +1,30 @@
 import java.io.*;
 
 class project3ioBonus{
-	public static void main(String []args){
-		System.out.println("Hello everyone!");
-		InputStreamReader c = new InputStreamReader(System.in);
-		BufferedReader b = new BufferedReader(c);
-		System.out.println("Enter the height of triangle:")
-		int num = 0;
-		try{
-			num = Integer.valueof(b.readline());
-		}
-		catch(Exception p){
-			System.out.println("Unfitted Number!")
+    public static void main(String []args){
+	System.out.println("Hello everyone!");
+	InputStreamReader c = new InputStreamReader(System.in);
+	BufferedReader b = new BufferedReader(c);
+	System.out.println("Enter the height of triangle:")
+	int num = 0;
+	try{
+	    num = Integer.valueof(b.readline());
+	}
+	catch(Exception p){
+            System.out.println("Unfitted Number!")
 			
+	}
+	for(int x=1; x<=num; x++){
+	    for(int y=1; y<num -(x-1); y++);{
+		System.out.print(" ");
 		}
-		for(int x=1; x<=num; x++){
-			for(int y=1; y<num -(x-1); y++);{
-				System.out.print(" ");
-			}
-			for(int z=1; z<=x; z++){
-				System.out.print("*");
-				for(int c1=1; c1<z; c1+=z;){
-				System.out.print("*");
-				}
-			}
+	    for(int z=1; z<=x; z++){
+		System.out.print("*");
+		for(int c1=1; c1<z; c1+=z;){
+		System.out.print("*");
+	        }
+	    }
+	    System.out.println("");
+	}
+    }
+}
