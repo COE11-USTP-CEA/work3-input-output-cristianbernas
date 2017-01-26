68979f751e9cdb9599a60cec21e7e0aef9cd5055
Cristian C. Bernas <xianbernas@gmail.com>
2017-01-26 13:56:04 +0800

    Add files via upload

---
+++ project3ioBonus.java
@@ -0,0 +1,26 @@
+import java.io.*;
+
+class project3ioBonus{
+	public static void main(String []args){
+		System.out.println("Hello everyone!");
+		InputStreamReader c = new InputStreamReader(System.in);
+		BufferedReader b = new BufferedReader(c);
+		System.out.println("Enter the desired height of triangle:")
+		int num = 0;
+		try{
+			num = Integer.valueof(c.readline());
+		}
+		catch(Exception c){
+			System.out.println("Unfitted Number!")
+			
+		}
+		for(int x=1; x<=num; x++){
+			for(int y=1; y<=num -(x-1); y++);{
+				System.out.print(" ");
+			}
+			for(int z=1; z<=x; z++){
+				System.out.print("*");
+				for(int c1=1; c1<z; c1+=z;){
+				System.out.print("*");
+				}
+			}
